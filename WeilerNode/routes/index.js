11d2f089aac3a9d1c7d4a61281c3296e5ac5a555
bradenza <btownrippleman>
2020-05-11 08:20:44 -0700

    added route for cards.html

--- WeilerNode/routes/index.js
+++ WeilerNode/routes/index.js
@@ -7,5 +7,9 @@ const router = express.Router();
 router.get('', (req, res) => {
     res.sendFile(path.join(__dirname + '/../index.html'));
 });
+router.get('/cards.html', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../cards.html'));
+});
+
 
 module.exports = router;
