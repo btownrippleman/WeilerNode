e4afeb20d5c2314691bdfc716e840dbf200b9dbc
bradenza <btownrippleman>
2020-05-11 08:45:49 -0700

    added for each loop to add each html file as a path, though html files with a space will not work

--- WeilerNode/routes/index.js
+++ WeilerNode/routes/index.js
@@ -7,8 +7,29 @@ const router = express.Router();
 router.get('', (req, res) => {
     res.sendFile(path.join(__dirname + '/../index.html'));
 });
-router.get('/cards.html', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../cards.html'));
+
+
+
+//requiring path and fs modules
+path = require('path');
+fs = require('fs');
+//joining path of directory 
+directoryPath = path.join(__dirname, '/..');
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        
+        if ((file.slice(file.indexOf("."))) == ".html"){
+            router.get('/'+file, (req, res) => {
+                res.sendFile(path.join(__dirname + '/../'+file));
+            });
+        } 
+    });
 });
 
 
